30990b7db5bcb9206fc21dee81aa0789d9d829ad
TitoAgudelo <titoarturoagudelo@gmail.com>
2015-05-14 11:44:09 -0500

    change in throw validation

--- cash.php
+++ cash.php
@@ -7,24 +7,25 @@
 	$result = array();
 	$notes = array(100, 50, 20, 10);
 	$count = 0;
-
-	if($cash < 10){
-		echo "throw InvalidArgumentException";
-	}
 	
-	do {
-		if(($cash / $notes[$count]) < 1) {
+	if($cash < 10 || ($cash % 10) > 0){
+		echo "throw InvalidArgumentException";
+	} 
+	else {
+		do {
+			if(($cash / $notes[$count]) < 1) {
+				++$count;
+				continue;
+			};
+			$result[$notes[$count]] = floor($cash / $notes[$count]);
+			$cash = $cash % $notes[$count];
 			++$count;
-			continue;
-		};
-		$result[$notes[$count]] = floor($cash / $notes[$count]);
-		$cash = $cash % $notes[$count];
-		++$count;
-	}
+		}
 
-	while($cash >= 10);
-	foreach($result as $note => $position)
-	{
-		echo $position . ' notes of ' . $note . ', ';
+		while($cash > 0);
+		foreach($result as $note => $position)
+		{
+			echo $position . ' notes of ' . $note . ', ';
+		}
 	}
 ?>
--- js/main.js
+++ js/main.js
@@ -35,3 +35,4 @@ app.controller('CashController', function($scope, $http) {
 		}
 	}
 });
+
